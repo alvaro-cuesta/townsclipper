a23bd5976701535fde788793c18eea5e00a335d3
Álvaro Cuesta <alvaro-cuesta@GitHub>
2020-08-27 23:50:51 +0200

    Full disasm?

--- docs/disasm2.cpp
+++ docs/disasm2.cpp
@@ -61,9 +61,9 @@ ulonglong Placemaker.TextSaveSystem$$LoadFromString
     } while ((int)uVar12 < saveString.length);
   }
 
-  uint something = bitLength
 
   char cVar2;
+  uint something = bitLength
   do {
     local_88 = uVar5;
     uVar12 = something;
@@ -73,24 +73,24 @@ ulonglong Placemaker.TextSaveSystem$$LoadFromString
 
   int readCount = 0
 
-  uint someBitLength = reader.read(this.bitArray, &readCount, 5);
-  uint local_78 = reader.read(this.bitArray, &readCount, 5);
-  undefined4 uVar4 = reader.read(this.bitArray, &readCount, 5);
+  uint posOffsetsBitLength = reader.read(this.bitArray, &readCount, 5);
+  uint yDeltaBitLength = reader.read(this.bitArray, &readCount, 5);
+  undefined4 xOffsetBitLengthPre = reader.read(this.bitArray, &readCount, 5);
 
-  if ((int)uVar12 < (int)(readCount + 1 + someBitLength * 2))
+  if ((int)uVar12 < (int)(readCount + 1 + posOffsetsBitLength * 2))
     throw ERR_NO_ROOM_VOXELS
 
-  int someIntOffset
-  if (someBitLength == 0) {
-    someIntOffset = 0;
+  int posOffsetsSignOffset
+  if (posOffsetsBitLength == 0) {
+    posOffsetsSignOffset = 0
   } else {
-    someIntOffset = 1 << ((char)someBitLength - 1U & 0x1f);
+    posOffsetsSignOffset = 1 << (posOffsetsBitLength - 1)
   }
 
-  int local_8c = reader.read(this.bitArray, &readCount, someBitLength);
-  local_8c = local_8c - someIntOffset;
-  int local_84 = reader.read(this.bitArray, &readCount, someBitLength);
-  local_84 = local_84 - someIntOffset;
+  int yInitial = reader.read(this.bitArray, &readCount, posOffsetsBitLength)
+    - posOffsetsSignOffset
+  int xInitial = reader.read(this.bitArray, &readCount, posOffsetsBitLength)
+    - posOffsetsSignOffset
 
   if (readCount + 4 > uVar12)
     throw ERR_NO_ROOM_TYPES
@@ -111,96 +111,68 @@ ulonglong Placemaker.TextSaveSystem$$LoadFromString
     } while (i < typeCount);
   }
 
-  uint uVar16 = 0;
+  uint typeIndexBitLength = 0;
   if (0 < typeCount + 1) {
     uVar5 = 1;
     do {
-      uVar16 = uVar16 + 1;
+      typeIndexBitLength = typeIndexBitLength + 1;
       uVar5 = uVar5 << 1 | (uint)((int)uVar5 < 0);
     } while ((int)uVar5 <= typeCount + 1);
   }
 
-  int iVar11 = (1 << ((byte)uVar16 & 0x1f)) + -1;
-  local_res8[0] = 1;
-  int iVar10 = local_8c;
-  int local_7c = iVar11;
-  undefined8 local_70 = CONCAT44(local_70._4_4_, uVar4); // local_70._4_4_ ?¿?¿?¿? undefined !!
-
-  byte local_res8 [8];
-  uint local_80;
+  int yPos = yInitial;
+  undefined8 xOffsetBitLength = CONCAT44(xOffsetBitLength._4_4_, xOffsetBitLengthPre); // xOffsetBitLength._4_4_ ?¿?¿?¿? undefined !!
   ushort local_94;
-  int iVar13;
-  byte bHeight;
+
+  bool isFirst = true;
+  int typeStopIndex = (1 << typeIndexBitLength) - 1;
 
   while (readCount < (int)local_88) {
-    if (local_res8[0] == 0) {
-      int local_1337 = reader.read(this.bitArray, &readCount, 1);
+    if (!isFirst) {
+      int isMoveY = reader.read(this.bitArray, &readCount, 1);
 
-      if (local_1337 == 1) {
-        int local_1338 = reader.read(this.bitArray, &readCount, (ulonglong)local_78);
-        iVar10 = iVar10 + local_1338;
-        local_8c = iVar10;
+      if (isMoveY == 1) {
+        int yDelta = reader.read(this.bitArray, &readCount, yDeltaBitLength);
+        yPos += yDelta;
       }
     } else {
-      local_res8[0] = 0;
+      isFirst = false;
     }
 
-    int local_1339 = reader.read(this.bitArray, &readCount, local_70 & 0xffffffff);
-    iVar13 = 0;
-    local_80 = local_1339 + local_84;
-    undefined8 local_68 = CONCAT44(local_80,iVar10);
+    uint xPos = xInitial
+      + reader.read(this.bitArray, &readCount, xOffsetBitLength);
 
-    int local_1340 = reader.read(this.bitArray, &readCount, 1);
-    if (local_1340 == 1) {
-      local_94 = 0xf;
-      System.Collections.Generic.List<ByteFloat2>$$Add(voxelList, 0xf, Method$System.Collections.Generic.List<SaveData.V>.Add());
-      iVar13 = 1;
+    int hasGround = reader.read(this.bitArray, &readCount, 1);
+    if (hasGround == 1) {
+      local_94 = 0x000f;
+      voxelList.push(0x000f)
     }
 
-    bHeight = 1;
+    byte height = 1;
 
     while( true ) {
-      int local_1341 = reader.read(this.bitArray, &readCount, (ulonglong)uVar16);
+      int typeIndex = reader.read(this.bitArray, &readCount, typeIndexBitLength);
 
-      if (local_1341 == iVar11)
+      if (typeIndex == typeStopIndex)
         break
 
-      if (local_1341 != 0) {
-        uVar5 = local_1341 - 1;
+      if (typeIndex != 0) {
+        uint typeIndexZeroBased = typeIndex - 1;
 
-        if ((typeCount <= (int)uVar5) || ((int)uVar5 < 0)) {
-          local_res8[0] = bHeight;
-
-          throw "Invalid voxel value {0}/{1} at {2} {3}".format(
-            <int>(uVar5),
-            <int>(typeCount),
-            <byte>(local_res8),
-            <Unity.Mathematics.int2>(local_68),
-          )
+        if (typeIndexZeroBased >= typeCount || typeIndexZeroBased < 0) {
+          throw "Invalid voxel value ${typeIndexZeroBased}/${typeCount} at ${bHeight} int2(${xPos}, ${yPos})"
         }
 
-        local_94 = local_94 & 0xff | (ushort)bHeight << 8;
-        byte bVar3 = FUN_180002290(typeList, (ulonglong)uVar5, Method$System.Collections.Generic.List<int>.get_Item());
-        local_94 = local_94 & 0xff00 | (ushort)bVar3;
-        System.Collections.Generic.List<ByteFloat2>$$Add(voxelList, (ulonglong)local_94);
+        voxelList.push([height, typeList[uVar5]])
 
-        iVar13 = iVar13 + 1;
-        iVar11 = local_7c;
-
-        if (bHeight == 0xff)
+        if (height == 0xff)
           break
       }
 
-      bHeight = bHeight + 1;
+      height += 1;
     }
 
-    iVar10 = local_8c;
-    uint uStack92 = local_80; // unused?
-    int local_60 = local_8c; // unused?
-    int local_40 = iVar13;  // unused?
-
-    undefined8 local_48 = CONCAT44(local_80, local_8c);
-    System.Collections.Generic.List<SaveData.C>$$Add(cornerList, &local_48);
+    cornerList.push([xPos, yPos])
   }
 
   return true
