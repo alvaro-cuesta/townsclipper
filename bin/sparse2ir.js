46a6ae45377e70b4344848382f3fd09bad4135f7
Álvaro Cuesta <alvaro-cuesta@GitHub>
2020-08-29 00:25:12 +0200

    Fix function call

--- bin/sparse2ir.js
+++ bin/sparse2ir.js
@@ -5,7 +5,7 @@ const { sparseToIR } = require('../lib/sparse')
 
 const sparse = JSON.parse(fs.readFileSync(process.stdin.fd, 'utf-8'))
 
-const ir = cornersToIR(sparse)
+const ir = sparseToIR(sparse)
 
 // TODO: Pretty flag
 process.stdout.write(JSON.stringify(ir, null, 2))
